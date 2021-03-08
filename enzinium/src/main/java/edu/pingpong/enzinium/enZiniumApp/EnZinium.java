64f516da6367f38fc8fd3b8c716726c5ebb0ce83
pau <pllinas@cifpfbmoll.eu>
2021-03-08 23:16:42 +0100

    feat: :sparkles: Creation directory structure

---
+++ enzinium/src/main/java/edu/pingpong/enzinium/enZiniumApp/EnZinium.java
@@ -0,0 +1,13 @@
+package edu.pingpong.enzinium.enZiniumApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class EnZinium 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
