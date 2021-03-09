78a55903c0ae7cf37a181db7a647da496488a26b
pau <pllinas@cifpfbmoll.eu>
2021-03-09 10:55:56 +0100

    test: :white_check_mark: Creation test cases for Address class

---
+++ enzinium/src/test/java/edu/pingpong/enzinium/AddressTest.java
@@ -0,0 +1,37 @@
+package edu.pingpong.enzinium;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AddressTest {
+
+    private Address address = new Address();
+    private double delta = 0.01;
+    
+    @Test
+    public void getBalanceTest() {
+        assertEquals(0 , address.getBalance(), delta);
+    }
+
+    @Test 
+    public void checkSkPresentTest() {
+        address.setPair();
+        assertTrue(address.checkSkPresent());
+    }
+
+    @Test
+    public void checkSkNotPresentTest() {
+        assertFalse(address.checkSkPresent());
+    }
+
+    @Test
+    public void checkPkNotNullTest() {
+        address.setPair();
+        assertNotNull(address.getPK());
+    }
+}
